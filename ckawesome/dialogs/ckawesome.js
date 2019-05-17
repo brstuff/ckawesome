3e1e0670e2ed70170277b7f85500459db1f5052a
Lane <lanecwheeler@gmail.com>
2019-05-16 23:36:58 -0500

    This is a FA5 version. Works (supposedly) with free and pro, just need to make sure you supply the fonts and css files

--- ckawesome/dialogs/ckawesome.js
+++ ckawesome/dialogs/ckawesome.js
@@ -1,57 +1,57 @@
 
 CKEDITOR.dialog.add('ckawesomeDialog', function( editor ) {
-	function getCKAwesomeIcons(selectList ){
-	     var result = [];
-	     var scriptUrl = editor.fontawesomePath;
-	     
-	     $.ajax({
-	        url: scriptUrl,
-	        type: 'get',
-	        dataType: 'html',
-	        async: false,
-	        success: function(response) {
-	        	var excludeStyles = [".fa.",".fa",".fa-lg",".fa-2x",".fa-3x",".fa-4x",".fa-5x",".fa-fw",".fa-ul",".fa-ul>",".fa-li",".fa-border",".fa-pull-left",".fa-pull-right",".fa-spin",".fa-pulse",".fa-rotate-90",".fa-rotate-180",".fa-rotate-270",".fa-flip-horizontal",".fa-flip-vertical",".fa-stack",".fa-stack-1x",".fa-stack-2x",".fa-inverse"];
-	        	var regxstyles = new RegExp(/\.[a-zA-Z_][\w-_]*[^\.\s\{#:\,;]/,"g" );
-	        	var styles = response.match(regxstyles);
-	        	styles.sort();
-	        	$.each(styles, function( index, value ) {
-	        		var xstart=value.substring(0, 3).substring(1);
-	        		if (xstart != 'fa' || excludeStyles.indexOf(value) > 0){ return; }
-	        		value = value.substring(1);
-	        		selectList.add(value, value);
-	        	})
+    function getCKAwesomeIcons(selectList ){
+        var result = [];
+        var scriptUrl = editor.fontawesomePath;
 
-	        },
-	        error: function (jqXHR, exception) {
-	            alert("Error loading Font Awesome css: \n" + scriptUrl);
-	        },
-	     });
-	}
+        $.ajax({
+            url: scriptUrl,
+            type: 'get',
+            dataType: 'html',
+            async: false,
+            success: function(response) {
+                var excludeStyles = [".fa.",".fa", ".fas", ".fab", ".fa-lg",".fa-1x",".fa-2x",".fa-3x",".fa-4x",".fa-5x",".fa-6x",".fa-7x",".fa-8x",".fa-9x",".fa-10x",".fa-fw",".fa-ul",".fa-ul>",".fa-li",".fa-border",".fa-pull-left",".fa-pull-right",".fa-spin",".fa-pulse",".fa-rotate-90",".fa-rotate-180",".fa-rotate-270",".fa-flip-horizontal",".fa-flip-vertical",".fa-stack",".fa-stack-1x",".fa-stack-2x",".fa-inverse"];
+                var regxstyles = new RegExp(/\.[a-zA-Z_][\w-_]*[^\.\s\{#:\,;]/,"g" );
+                var styles = response.match(regxstyles);
+                styles.sort();
+                $.each(styles, function( index, value ) {
+                    var xstart=value.substring(0, 3).substring(1);
+                    if (xstart != 'fa' || excludeStyles.indexOf(value) > 0){ return; }
+                    value = value.substring(1);
+                    selectList.add(value, value);
+                })
 
-	function getSelectionOptions(selectList, start, inc, many){
-	    var result = [];
-	    var val = start; 
-	    
-	    result.push(start);
-	    
-	    many = many > 0 ? many : 5;
-	    for(var i = 0; i < many; i++){
-	    	val += inc;
-	    	result.push(val);
-	    }
-	    
-	    $.each(result, function( index, value ) {
-	   		selectList.add(value, value);
-	   	})
-	}
+            },
+            error: function (jqXHR, exception) {
+                alert("Error loading Font Awesome css: \n" + scriptUrl);
+            },
+        });
+    }
 
-	function formatCKAwesome (icon) {
-	  if (!icon.id) { return icon.text; }
-	  var text = icon.text.replace(/fa-|\.|\-/gi, " ");
-	  var icon = $('<span class="ckawesome_options"><i class="fa ' + icon.element.value + ' fa-fw"></i> ' + text + "</span>");
-	  return icon;
-	};
-	
+    function getSelectionOptions(selectList, start, inc, many){
+        var result = [];
+        var val = start;
+
+        result.push(start);
+
+        many = many > 0 ? many : 5;
+        for(var i = 0; i < many; i++){
+            val += inc;
+            result.push(val);
+        }
+
+        $.each(result, function( index, value ) {
+            selectList.add(value, value);
+        })
+    }
+
+    function formatCKAwesome (icon) {
+        if (!icon.id) { return icon.text; }
+        var text = icon.text.replace(/fa-|\.|\-/gi, " ");
+        var icon = $('<span class="ckawesome_options"><i class="fa ' + icon.element.value + ' fa-fw"></i> ' + text + "</span>");
+        return icon;
+    };
+
     return {
         title: 'Insert CKAwesome',
         minWidth: 200,
@@ -63,31 +63,38 @@ CKEDITOR.dialog.add('ckawesomeDialog', function( editor ) {
                 label: 'Basic Settings',
                 elements: [
                     {
-					    type: 'select',
-					    id: 'ckawesomebox',
-					    label: 'Select font Awesome',
-					    validate: CKEDITOR.dialog.validate.notEmpty( "Font Awesome field cannot be empty." ),
-					    items: [[ editor.lang.common.notSet, '' ]],
-					    onLoad: function () {
-						   	getCKAwesomeIcons(this);
-						   	var selectbx = $('#' + this.getInputElement().getAttribute('id'));  
-						   	$(selectbx).select2({ width: "100%", templateResult: formatCKAwesome, templateSelection: formatCKAwesome});
-					    },
-					    onShow: function(){
-					    	var selectbx = $('#' + this.getInputElement().getAttribute('id'));  
-					    	$(selectbx).val('').trigger('change') ;
-					    }
+                        type: 'select',
+                        id: 'ckawesomebox',
+                        label: 'Select Font Awesome Icon',
+                        validate: CKEDITOR.dialog.validate.notEmpty( "Icon cannot be empty." ),
+                        items: [[ editor.lang.common.notSet, '' ]],
+                        onLoad: function () {
+                            getCKAwesomeIcons(this);
+                            var selectbx = $('#' + this.getInputElement().getAttribute('id'));
+                            $(selectbx).select2({ width: "100%", templateResult: formatCKAwesome, templateSelection: formatCKAwesome});
+                        },
+                        onShow: function(){
+                            var selectbx = $('#' + this.getInputElement().getAttribute('id'));
+                            $(selectbx).val('').trigger('change') ;
+                        }
+                    },
+                    {
+                        type: 'select',
+                        id: 'ckawesomepro',
+                        label: "If you're using the Pro version, you can choose your style modifier here",
+                        validate: CKEDITOR.dialog.validate.notEmpty( "Pro option cannot be empty." ),
+                        items: [['Not Using Pro', ''],[ 'Solid', 'fas'],['Regular', 'far'], ['Light', 'fal']], 'default' : ''
                     },
                     {
                         type: 'select',
                         id: 'textsize',
-                        label: 'Select  size',
+                        label: 'Select Size',
                         items: [[ editor.lang.common.notSet, '' ]],
                         onLoad: function (widget) {
-                        	getSelectionOptions(this, 8, 1, 42);
+                            getSelectionOptions(this, 8, 4, 16);
                         }
                     },
-                    {   
+                    {
                         type: "hbox",
                         padding: 0,
                         widths: ["80%", "20%"],
@@ -95,19 +102,19 @@ CKEDITOR.dialog.add('ckawesomeDialog', function( editor ) {
                             {
                                 id: 'fontcolor',
                                 type: 'text',
-                                label: 'Select color',
+                                label: 'Select Color',
                                 onChange: function( element ) {
-                                	var idEl = $('#' +this.getInputElement().getAttribute('id'));
-                                	idEl.css("background-color", idEl.val());
+                                    var idEl = $('#' +this.getInputElement().getAttribute('id'));
+                                    idEl.css("background-color", idEl.val());
                                 },
                                 onKeyUp: function( element ) {
-                                	var idEl = $('#' + this.getInputElement().getAttribute('id'));
-                                	idEl.css("background-color", idEl.val());
+                                    var idEl = $('#' + this.getInputElement().getAttribute('id'));
+                                    idEl.css("background-color", idEl.val());
                                 },
-        					    onShow: function(){
-        					    	var idEl = $('#' + this.getInputElement().getAttribute('id'));  
-                                	idEl.css("background-color", "");
-        					    }
+                                onShow: function(){
+                                    var idEl = $('#' + this.getInputElement().getAttribute('id'));
+                                    idEl.css("background-color", "");
+                                }
                             },
                             {
                                 type: "button",
@@ -126,6 +133,13 @@ CKEDITOR.dialog.add('ckawesomeDialog', function( editor ) {
                                 }
                             }
                         ]
+                    },
+                    {
+                        type: 'select',
+                        id: 'alignment',
+                        label: "Icon Vertical Alignment",
+                        validate: CKEDITOR.dialog.validate.notEmpty( "Alignment cannot be empty." ),
+                        items: [[ 'Normal', 'unset'], ['Middle', 'middle']], 'default' : 'unset' ,
                     }
                 ]
             },
@@ -137,9 +151,10 @@ CKEDITOR.dialog.add('ckawesomeDialog', function( editor ) {
 
             var cka_size = dialog.getValueOf( 'options', 'textsize' );
             var cka_color = dialog.getValueOf( 'options', 'fontcolor' );
-            var cka_class = "fa fa-fw " + dialog.getValueOf( 'options', 'ckawesomebox' );
-            var cka_style = ( cka_size != '' ? 'font-size: '+cka_size+'px;' : '' ) + ( cka_color != '' ? 'color: '+cka_color+';' : '' ) ;
-            
+            var cka_alignment = dialog.getValueOf( 'options', 'alignment' );
+            var cka_class = "fa fa-fw " + dialog.getValueOf( 'options', 'ckawesomebox' ) + " " + dialog.getValueOf( 'options', 'ckawesomepro' );
+            var cka_style = ( cka_size != '' ? 'font-size: '+cka_size+'px;' : '' ) + ( cka_color != '' ? 'color: '+cka_color+';' : '' ) + ( cka_alignment != '' ? 'vertical-align: '+cka_alignment+';' : '' ) ;
+
             cka.setAttribute( 'class', cka_class );
             if ( cka_style ) cka.setAttribute( 'style', cka_style );
 
